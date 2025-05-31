701f1538dd4d7b9c091f52db396431f6c8f90729
mine <resetlol2004@gmail.com>
2025-05-31 19:58:09 +0300

    fix link and missing package init

--- outputs/file_output.py
+++ outputs/file_output.py
@@ -4,7 +4,7 @@ from services.file_output_processor import FileOutputProcessor
 
 def file_output(data: OutputData):
     while True:
-        print("Введіть шлях файлу для збереження результатів (наприклад, 'c:\Users\customer\Documents'):")
+        print("Введіть шлях файлу для збереження результатів (наприклад, 'c:\\Users\\customer\\Documents'):")
         file_path = input().strip()
         try:
             processor = FileOutputProcessor(file_path, data)
