a7c12d591803b3af47a8a432a2a517136ffbff73
Zhayvoronok Kateryna <1gayv425@gmail.com>
2025-05-31 18:51:17 +0300

    added gale_shapley algorithm

--- algs/gale_shapley.py
+++ algs/gale_shapley.py
@@ -3,6 +3,36 @@ from objects.InputData import InputData
 
 
 def gale_shapley_algorithm(input_data: InputData) -> List[List[int]]:
-    x = []
+    m = len(input_data.discipline_capacities)
+    n = len(input_data.students)
+
+    x = [[0 for _ in range(n)] for _ in range(m)]
+    H = {i: [] for i in range(m)}
+    overflow = [False for _ in range(m)]
+
+    wishlists = {s: list(input_data.wishlists[s]) for s in input_data.students}
+    queue = list(input_data.students.keys())
+
+    while queue:
+        student = queue.pop(0)
+
+        if not wishlists[student]:
+            continue
+
+        d = wishlists[student][0]
+
+        H[d].append(student)
+        H[d].sort(key=lambda s: input_data.students[s], reverse=True)
+
+        if overflow[d] or len(H[d]) > input_data.discipline_capacities[d]:
+            last = H[d].pop()
+            queue.append(last)
+            overflow[d] = True
+
+        wishlists[student].pop(0)
+
+    for d in range(m):
+        for student in H[d]:
+            x[d][student] = 1
 
     return x
